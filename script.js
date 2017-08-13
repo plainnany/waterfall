39bf9eb0ad30ab0fe13f8999162e8d9c6950b747
plainnany <plainnany@gmail.com>
2017-08-13 21:40:00 +0800

    修改协议

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ function isVisible(element){
 function getData(callback){
     
     $.ajax({
-        url: 'http://platform.sina.com.cn/slide/album_tech',
+        url: '//platform.sina.com.cn/slide/album_tech',
         dataType: 'jsonp',
         jsonp: 'jsoncallback',
         data: {
